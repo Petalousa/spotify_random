802b20d2ac6c0a1ccb2b6ded2127b4b042eec206
Elijah MacDonald <terrapetalousa@gmail.com>
2025-03-22 13:07:04 -0400

    Add open in spotify bubble.

--- frontend/src/components/Tile.tsx
+++ frontend/src/components/Tile.tsx
@@ -6,8 +6,10 @@ type Props = {
 
 export default function Tile({ songName, artistName, spotifyUri }: Props) {
     const tileUrl = "https://open.spotify.com/track/" + spotifyUri;
+    const spotifyAppUrl = "spotify:track:" + spotifyUri;
     return (
     <a href={tileUrl} target="_blank">
+        <a href={spotifyAppUrl}><div className="bubble" title="Open in Spotify">♫</div></a>
         <div className="card" style={{width: "calc(25% - 15px);"}}>
             <div>{songName}</div>
             <div><i>{artistName}</i></div>
